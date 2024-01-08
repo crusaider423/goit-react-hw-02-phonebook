1ae26121ff7f73883e1c2cf6f5e470fe1d0b6e0b
Олександр Канюка <137859612+crusaider423@users.noreply.github.com>
2024-01-08 17:22:25 +0200

    update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -11,7 +11,7 @@ export class ContactForm extends Component {
 
   handleChange = ({ target: { name, value } }) => {
     this.setState({
-      [name]: value.trim(),
+      [name]: value,
     });
   };
   handleSubmit = e => {
